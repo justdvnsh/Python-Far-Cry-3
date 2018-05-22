8b81c892bb0f3f4bd3354f80ef9f6c2b88db14dc
Divyansh Dwivedi <justdvnsh2208@gmail.com>
2018-05-22 18:05:23 +0530

    image processing

--- getting-visuals.py
+++ getting-visuals.py
@@ -3,17 +3,27 @@ from PIL import ImageGrab
 import cv2
 import time
 
+
+def process_img(image):
+    original_image = image
+    # convert to gray
+    processed_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # edge detection
+    processed_img =  cv2.Canny(processed_img, threshold1 = 200, threshold2=300)
+    return processed_img
+
 def screen_record(): 
     last_time = time.time()
-    while(True):
-        # 800x600 windowed mode for GTA 5, at the top left position of your main screen.
-        # 40 px accounts for title bar. 
-        printscreen =  np.array(ImageGrab.grab(bbox=(0,40,1280,600)))
-        print('loop took {} seconds'.format(time.time()-last_time))
+    while True:
+        screen =  np.array(ImageGrab.grab(bbox=(0,40,800,640)))
+        #print('Frame took {} seconds'.format(time.time()-last_time))
         last_time = time.time()
-        cv2.imshow('window',cv2.cvtColor(printscreen, cv2.COLOR_BGR2RGB))
+        new_screen = process_img(screen)
+        cv2.imshow('window', new_screen)
+        #cv2.imshow('window',cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             break
+
 
 screen_record()
